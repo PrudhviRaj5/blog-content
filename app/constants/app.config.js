ac7db985e46e009cd9a9ddf50939d5492b5a361c
PrudhviRaj5 <raj.prudhvi5@gmail.com>
2019-02-01 00:10:51 +0530

    pushing react build with prod url

--- app/constants/app.config.js
+++ app/constants/app.config.js
@@ -2,4 +2,4 @@ const PORT = 5500;
 const PROD_URL = 'https://raw.githubusercontent.com/PrudhviRaj5/my-awesome-blog/master';
 const DEV_URL = `http://localhost:${PORT}`;
 
-export const URL = process.env.NODE_ENV === 'prodution' ? PROD_URL : DEV_URL;
+export const URL = process.env.NODE_ENV === 'production' ? PROD_URL : DEV_URL;
