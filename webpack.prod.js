ea6ac093e346ee2ed0d96dafc60b7a2e8ccec9e7
PrudhviRaj5 <raj.prudhvi5@gmail.com>
2019-04-20 13:48:50 +0530

    update webpack prod config with terser and compress plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,10 +2,11 @@ const { resolve } = require('path');
 const webpack = require('webpack');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const commonConfig = require('./webpack.common');
+const CompressionPlugin = require('compression-webpack-plugin');
 
+const commonConfig = require('./webpack.common');
 
 const config = {
   mode: 'production',
@@ -23,29 +24,60 @@ const config = {
   },
 
   optimization: {
-    runtimeChunk: false,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
+    nodeEnv: 'production',
+    sideEffects: false, // changed to false for material-web-components
+    concatenateModules: true,
+    occurrenceOrder: true,
+    // runtimeChunk: true,
+    minimize: true,
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({}),
+      new TerserPlugin({
+        parallel: true,
+        cache: true,
+        sourceMap: true,
+        terserOptions: {
+          warnings: false,
+          compress: {
+            comparisons: false,
+          },
+          parse: {},
+          mangle: true,
+          output: {
+            comments: false,
+            ascii_only: true,
+          },
         },
+      }),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      minSize: 0,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 10,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/,
+            )[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+        },
+        // default: {
+        //   minChunks: 2,
+        //   priority: -20,
+        //   reuseExistingChunk: true,
+        //   enforce: true,
+        // },
       },
     },
-    minimizer: [
-      new UglifyJsPlugin({
-        cache: true,
-        parallel: true,
-        sourceMap: true,
-      }),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
   },
 };
 
 const prodPlugins = [
-  new CleanWebpackPlugin(['dist']),
+  new CleanWebpackPlugin(),
   new webpack.optimize.OccurrenceOrderPlugin(),
   new webpack.LoaderOptionsPlugin({
     minimize: true,
@@ -54,6 +86,13 @@ const prodPlugins = [
   new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
   new MiniCssExtractPlugin({
     filename: 'styles/[name].bundle.css',
+  }),
+  new CompressionPlugin({
+    algorithm: 'gzip',
+    test: /\.(js|css|html|svg)$/,
+    threshold: 10240,
+    compressionOptions: { level: 6 },
+    minRatio: 0.8,
   }),
 ];
 
